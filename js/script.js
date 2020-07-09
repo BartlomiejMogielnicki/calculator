ca12d45f5612bdab6b36a95ee90441f1543bb366
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-07-09 20:04:54 +0200

    Input numbers to screen

--- js/script.js
+++ js/script.js
@@ -2,5 +2,43 @@ const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const clearButton = document.querySelector('[data-clear]');
-const previousOperand = document.querySelector('[data-previous-operand]');
-const currentOperand = document.querySelector('[data-current-operand]');
+const previousOperandEl = document.querySelector('[data-previous-operand]');
+const currentOperandEl = document.querySelector('[data-current-operand]');
+
+let previousOperand, currentOperand, operation;
+
+const clear = () => {
+    previousOperand = '';
+    currentOperand = '';
+    operation = undefined;
+};
+
+clear();
+
+const inputNumber = (number) => {
+    if (number === ',' && currentOperand.includes(',')) {
+        return;
+    }
+    currentOperand = currentOperand.toString() + number.toString();
+};
+
+const pickOperation = () => {
+
+};
+
+const compute = () => {
+
+};
+
+const updateScreen = () => {
+    previousOperandEl.innerHTML = previousOperand;
+    currentOperandEl.innerHTML = currentOperand;
+};
+
+// Event listeners
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        inputNumber(button.innerHTML);
+        updateScreen();
+    });
+});
