2d4c85a5783a61249ef68faf13a666e572a039bd
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-07-09 20:56:56 +0200

    Add percent button functionality

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const clearButton = document.querySelector('[data-clear]');
-const swapSign = document.querySelector('[data-swap]');
+const swapSignButton = document.querySelector('[data-swap]');
+const percentButton = document.querySelector('[data-percent]');
 const previousOperandEl = document.querySelector('[data-previous-operand]');
 const currentOperandEl = document.querySelector('[data-current-operand]');
 
@@ -11,7 +12,7 @@ let previousOperand, currentOperand, operation;
 const clear = () => {
     previousOperand = '';
     currentOperand = '';
-    operation = undefined;
+    operation = '';
 };
 
 clear();
@@ -59,9 +60,6 @@ const compute = () => {
         case 'x':
             computation = prevNumber * currentNumber;
             break;
-        case '%':
-            computation = (prevNumber / currentNumber) * 100;
-            break;
         default:
             return;
     };
@@ -70,10 +68,19 @@ const compute = () => {
     previousOperand = '';
 };
 
+// Compute percent
+const computePercent = () => {
+    currentOperand = (previousOperand * currentOperand) / 100;
+};
+
 // Update previous and current operands on the screen
 const updateScreen = () => {
-    previousOperandEl.innerHTML = previousOperand;
     currentOperandEl.innerHTML = currentOperand;
+    if (operation != null) {
+        previousOperandEl.innerHTML = `${previousOperand} ${operation}`
+    } else {
+        previousOperandEl.innerHTML = previousOperand;
+    };
 };
 
 // Swap sign
@@ -112,6 +119,12 @@ clearButton.addEventListener('click', () => {
 });
 
 // Swap sign button
-swapSign.addEventListener('click', () => {
+swapSignButton.addEventListener('click', () => {
     swapNumberSign();
+});
+
+// Percent button
+percentButton.addEventListener('click', () => {
+    computePercent();
+    updateScreen();
 });
